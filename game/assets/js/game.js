8c92e9ad6ade9bec74b30af4c0f3a33aaa219650
txtony9 <txtony9@penguin>
2023-10-08 04:11:41 -0700

    added quesations to JS

---
+++ game/assets/js/game.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "whick choice is a semantic element?",
+        choices:["div","span","header","class"],
+        answer: "header"
+    },
+    {
+        title: "what is the full form of CSS?",
+        choices:["Coloured Special Sheets","Color and Style Sheet","Cascading Style Sheets","Color Some Stuff"],
+        answer: "Casacading Style Sheets"
+    },
+    {
+        title: "What keyword do we use to define variable in JavaScript?",
+        choices:["div","for","var","none of the above"],
+        answer: "var"
+    },
+    {
+        title: "commonly used data types DO NOT include?",
+        choices:["strings","booleans","alerts","numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "which choice is a looping structure in JavaScript?",
+        choices:["var","if","for","const"],
+        answer: "for"
+    },
+];
+
